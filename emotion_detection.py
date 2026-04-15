730d6c0cc1467425915edba92babe25dd0041992
Raul Prior <raul.prior.g@gmail.com>
2026-04-15 19:15:22 -0400

    task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,30 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers = headers)
-    return response.text
+    jsonResponse = json.loads(response.text)
+    emotions = jsonResponse['emotionPredictions'][0]
+    emotion_dic = {
+        'anger': emotions['emotion']['anger'],
+        'disgust': emotions['emotion']['disgust'],
+        'fear': emotions['emotion']['fear'],
+        'joy': emotions['emotion']['joy'],
+        'sadness': emotions['emotion']['sadness']
+    }
+    
+    values = []
+    for val in emotion_dic.values():
+        values.append(float(val))
+    maxVal = max(values)
+
+    for key, value in emotion_dic.items():
+        if float(value) == maxVal:
+            maxKey = key
+
+    emotion_dic['dominant_emotion'] = maxKey
+
+    return emotion_dic
